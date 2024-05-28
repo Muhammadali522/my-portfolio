4cf6d8d28a5cdb933f57a237ec4a02d7b00adf2f
Muhammadali522 <muhammadaliakmal522@gmail.com>
2024-05-29 00:15:40 +0500

    my project

--- app.js
+++ app.js
@@ -11,3 +11,41 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+document.querySelector('.contact-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const name = this.querySelector('input[name="name"]').value;
+    const email = this.querySelector('input[name="email"]').value;
+    const subject = this.querySelector('input[name="subject"]').value;
+    const message = this.querySelector('textarea[name="message"]').value;
+
+    const telegramBotToken = '7090576897:AAH3vxzJe8L4Cp0IOLfMF_Kr36EeWzhJsiM';
+    const chatId = '1406491528';
+    const telegramApiUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+
+    const text = `New message from contact form:\n\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
+
+    try {
+        const response = await fetch(telegramApiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: text
+            })
+        });
+
+        if (response.ok) {
+            alert('Message sent successfully!');
+            
+            this.reset();
+        } else {
+            throw new Error('Failed to send message.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Failed to send message.');
+    }
+});
